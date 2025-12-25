674350c449835367c1cf2110cdd344861f4c38d1
TruBrooks <trumanbrooksy@gmail.com>
2025-12-25 01:46:35 -0800

    furthered along the current marching cubes impl

--- weave/src/marching_computed.rs
+++ weave/src/marching_computed.rs
@@ -17,6 +17,7 @@ use bytemuck::{Pod, Zeroable};
 
 pub const FIELD_SIZE: u32 = crate::chunks::CHUNK_SIZE + 1;
 pub const WORK_GROUP_SIZE: u32 = 4;
+pub const TOTAL_WORK_GROUP_SIZE: u32 = (FIELD_SIZE + WORK_GROUP_SIZE - 1) / WORK_GROUP_SIZE;
 pub const MAX_VERTS: usize = 65535;
 
 pub fn plugin(app: &mut App) {
@@ -133,15 +134,20 @@ pub struct MarchVertex {
 // Data Management
 //
 
-#[derive(Resource, ExtractResource, Clone, Default)]
-pub struct MainWorldQueue {
-    pub queue: Vec<Request>,
-}
+/// Every Request in the queue is processed and cleared
+/// before the next update (render world time)
+/// Duplicate requests aren't handled by me (as of Dec 25. 2025)
+/// or and of my sub-processes
+#[derive(Resource, ExtractResource, Clone, Default, Deref, DerefMut)]
+pub struct MainWorldQueue(Vec<Request>);
 
+// Could be an event
+// Currently doesn't allow you to return density values
+// Something to think about if thats needed
+// don't want the extra layer of complexity for now
 #[derive(Clone)]
 pub enum Request {
-    Density(NoiseParams),
-    Mesh(Density),
+    Mesh((NoiseParams, Density)),
     Full(NoiseParams),
 }
 
@@ -149,27 +155,18 @@ pub type Density = Vec<f32>;
 
 impl MainWorldQueue {
     pub fn new() -> Self {
-        Self { queue: Vec::new() }
+        Self(Vec::new())
     }
 
+    /// What ever you queue will be cleared the next frame and not accesible
     pub fn queue(&mut self, request: Request) {
-        self.queue.push(request);
+        self.push(request);
     }
 }
 
-impl Queue for MainWorldQueue {
-    fn clear(&mut self) {
-        self.queue.clear();
-    }
-}
-
-pub trait Queue: Resource {
-    fn clear(&mut self);
-}
-
-pub fn clear_queue<T: Queue>(mut queue: ResMut<T>) {
-    queue.clear();
-}
+/// Mesnt to exist strictly in the render world
+#[derive(Resource, Deref, DerefMut)]
+pub struct ComputeQueue(Vec<Buffer>);
 
 //
 // Render Graph Node
@@ -186,11 +183,91 @@ impl render_graph::Node for ComputeNode {
         render_context: &mut RenderContext,
         world: &World,
     ) -> Result<(), render_graph::NodeRunError> {
-        let queue = world.resource::<MainWorldQueue>();
+        let main_queue = world.resource::<MainWorldQueue>();
         let device = world.resource::<RenderDevice>();
         let cache = world.resource::<PipelineCache>();
         let pipelines = world.resource::<ComputePipelines>();
         let buffers = world.resource::<RenderAssets<GpuShaderStorageBuffer>>();
+
+        // the following set of lines could look nicer
+        let density_compute = cache.get_compute_pipeline(pipelines.density_pipeline);
+        let mesh_compute = cache.get_compute_pipeline(pipelines.mesh_pipeline);
+        if density_compute.is_none() || mesh_compute.is_none() {
+            warn!("Pipelins aren't ready!");
+            return Ok(());
+        }
+
+        // The main_queue is cleared in main each frame
+        for request in main_queue.iter() {
+            match request {
+                // User set density field mesh
+                Request::Mesh((noise_params, data)) => {
+                    // It seems this likely isn't needed
+                    let mut pass = render_context.command_encoder().begin_compute_pass(
+                        &ComputePassDescriptor {
+                            label: Some("User set density field mesh"),
+                            ..default()
+                        },
+                    );
+
+                    let params_buf = device.create_buffer_with_data(&BufferInitDescriptor {
+                        label: Some("User set density field params buffer"),
+                        contents: bytemuck::cast_slice(&[*noise_params]),
+                        usage: BufferUsages::UNIFORM,
+                    });
+                    // TODO!: can't figure out the contents sitch.
+                    //let density_buf = device.create_buffer_with_data(&BufferInitDescriptor {
+                    //    label: Some("User set density data buffer"),
+                    //    contents: data,
+                    //    usage: BufferUsages::STORAGE,
+                    //});
+                    todo!("This is unimplemented!");
+                }
+                // Generated density field + mesh generation
+                Request::Full(noise_params) => {
+                    let mut pass = render_context.command_encoder().begin_compute_pass(
+                        &ComputePassDescriptor {
+                            label: Some("Generate Density Values"),
+                            ..default()
+                        },
+                    );
+
+                    let params_buf = device.create_buffer_with_data(&BufferInitDescriptor {
+                        label: Some("User set density field params buffer"),
+                        contents: bytemuck::cast_slice(&[*noise_params]),
+                        usage: BufferUsages::UNIFORM,
+                    });
+
+                    let density_buf = device.create_buffer(&BufferDescriptor {
+                        label: Some("Generated Density values"),
+                        size: (FIELD_SIZE * FIELD_SIZE * FIELD_SIZE * 4) as u64,
+                        usage: BufferUsages::STORAGE,
+                        mapped_at_creation: true,
+                    });
+                    todo!("This goes no where and the data is never used!");
+                    // This should be attached to a buffer/handle which can be carried -
+                    // when done - to the next shader... research on bevy or other things on
+                    // how to do this
+                    // Note: I'm likely wrong about this impl, the docs are rough
+                    let bind_group = device.create_bind_group(
+                        "Generated Desnity values",
+                        &pipelines.density_layout,
+                        &BindGroupEntries::sequential((
+                            params_buf.as_entire_binding(),
+                            density_buf.as_entire_binding(),
+                        )),
+                    );
+
+                    pass.set_bind_group(0, &bind_group, &[]);
+                    pass.set_pipeline(density_compute.unwrap());
+                    pass.dispatch_workgroups(
+                        TOTAL_WORK_GROUP_SIZE,
+                        TOTAL_WORK_GROUP_SIZE,
+                        TOTAL_WORK_GROUP_SIZE,
+                    );
+                }
+            }
+        }
 
         Ok(())
     }
